bb9418fa9b5227a83f2eb5dbd2dbbdb46b101dea
Konrad Kollnig <5175206+kasnder@users.noreply.github.com>
2026-09-06 14:09:21 +0200

    Adapt held TCP test harness to DNS replay API

--- app/src/test/native/tcp_half_close_test.c
+++ app/src/test/native/tcp_half_close_test.c
@@ -171,6 +171,12 @@ void parse_dns_response(const struct arguments *args, const struct ng_session *s
     (void) datalen;
 }
 
+void record_dns_response(const struct arguments *args, uint8_t *data, size_t datalen) {
+    (void) args;
+    (void) data;
+    (void) datalen;
+}
+
 void parse_dns_partial_response(const struct arguments *args,
                                 const struct ng_session *session,
                                 uint8_t *data, size_t *datalen, int *blanked) {
